6536465952f1fd2e89f0c1b5beafbb8e22288b5b
saqermybot <Alhossiniabdulhalim2@gmail.com>
2026-01-03 22:16:44 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 import { UI } from "./js/ui.js";
-import { initAuth } from "./js/auth.js";
-import { initCategories } from "./js/categories.js";
 import { initListings } from "./js/listings.js";
 import { initAddListing } from "./js/addListing.js";
+import { initAuth } from "./js/auth.js";
+import { initCategories } from "./js/categories.js";
 import { initChat } from "./js/chat.js";
 
 UI.init();
 
+// ✅ جهّز actions أولاً قبل auth
+initListings();
 initAddListing();
+
 initAuth();
 await initCategories();
-initListings();
 initChat();
 
 // أول تحميل
