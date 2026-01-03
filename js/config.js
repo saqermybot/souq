4c96ee954250ac709a4c29c8c26edff4665a2197
saqermybot <Alhossiniabdulhalim2@gmail.com>
2026-01-03 16:15:35 +0100

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,21 @@
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAdZJuYm9tZ9H8Bw60zlzGy-Igt-qub0D8",
+  authDomain: "souq-a0e16.firebaseapp.com",
+  projectId: "souq-a0e16",
+  storageBucket: "souq-a0e16.firebasestorage.app",
+  messagingSenderId: "380896713624",
+  appId: "1:380896713624:web:22b11fc1a19c93af85e9ed",
+  measurementId: "G-WZC4VY0HK8"
+};
+
+// Cloudinary (Unsigned upload preset)
+export const CLOUDINARY = {
+  cloudName: "dr59awqcq",
+  uploadPreset: "souq_unsigned",
+  folder: "souq/listings"
+};
+
+export const SY_CITIES = [
+  "دمشق","ريف دمشق","حمص","حماة","حلب","اللاذقية","طرطوس","إدلب",
+  "دير الزور","الرقة","الحسكة","درعا","السويداء","القنيطرة"
+];
